190c0fe318c74c644835c39ea834f118135524ff
Gaurav Sekhri <gauravsekhri69@gmail.com>
2024-12-19 10:25:29 +0530

    cookie change 4

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -38,9 +38,10 @@ export class UserService {
         });
 
         response.cookie('auth', token, {
-          secure: true,
+          // secure: true,
           httpOnly: true,
           sameSite: 'none',
+          domain: 'https://jc-dashboard-frontend.vercel.app',
           maxAge: parseInt(process.env.COOKIE_MAXAGE),
         });
 
